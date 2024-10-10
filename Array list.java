a3536806166dfea97b3c8b7de377c4c199219390
Naggayi2023 <nassazikasule2023@gmail.com>
2024-10-10 13:19:35 +0300

    Create Array list.java

---
+++ Array list.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class DisplayArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        // Add elements
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        // Display elements
+        System.out.println("Numbers: " + numbers);
+    }
+}
